b4415bb3fd7865ab050535cabb6b520bebc46c64
UjeNeTORT <tikhonovty@gmail.com>
2024-04-02 21:57:18 +0300

    hash table data structure and basic functions (ctor & dtor)

--- hash_table/hash_table.cpp
+++ hash_table/hash_table.cpp
@@ -1,3 +1,37 @@
 #include "hash_table.h"
 
-HashTable *HashTableCtor();
+HashTable *HashTableCtor (
+    size_t hash_table_size,
+    volatile u_int64_t (* hash_func_ptr)
+            (const char * const word, size_t word_length))
+{
+    HashTable *hash_table = (HashTable *) calloc (1, sizeof (HashTable));
+
+    hash_table->table = (List **) calloc (hash_table_size, sizeof (List *));
+
+    for (int i = 0; i < hash_table_size; i++)
+        hash_table->table[i] = ListCtor (DEFAULT_LIST_SIZE);
+
+    hash_table->size = hash_table_size;
+
+    hash_table->hash_func_ptr = hash_func_ptr;
+
+    return hash_table;
+}
+
+void HashTableDtor (HashTable *hash_table)
+{
+    assert (hash_table);
+
+    for (int i = 0; i < hash_table->size; i++)
+        ListDtor (hash_table->table[i]);
+
+    hash_table->size = 0;
+    hash_table->hash_func_ptr = NULL;
+
+    free (hash_table->table);
+    free (hash_table);
+
+    return; // void
+}
+
--- hash_table/hash_table.h
+++ hash_table/hash_table.h
@@ -1,19 +1,30 @@
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "../common/common.h"
+#include "../linked_list/super_list.h"
+
+const size_t DEFAULT_LIST_SIZE = 1;
 
 struct HashTable
 {
-
+    List    **table;
+    size_t  size;
+    volatile u_int64_t (*hash_func_ptr)
+            (const char * const word, size_t word_length);
 };
 
-HashTable *HashTableCtor ();
-void       HashTableDtor ();
+HashTable *HashTableCtor   (size_t hash_table_size,
+                            volatile u_int64_t (* hash_func_ptr)
+                                    (const char * const word, size_t word_length));
+
+void       HashTableDtor   (HashTable *hash_table);
 
 int        HashTableVerifier ();
 int        HashTableDump     ();
 
-
-
+#endif // HASH_TABLE_H
