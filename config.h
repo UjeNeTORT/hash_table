c48a3cafe7561acd1ccb7f06b24b68c363bbb5de
UjeNeTORT <tikhonovty@gmail.com>
2024-04-06 14:56:23 +0300

    upd cfg file

--- config.h
+++ config.h
@@ -1,18 +1,18 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-typedef const char * const ht_key_t;
+typedef const char * ht_key_t;
 typedef volatile u_int64_t (* hash_func_ptr_t)(ht_key_t key);
 typedef u_int64_t          hash_t;
 
 typedef struct
 {
-    ht_key_t word;
+    ht_key_t key; //< key of hash table
     int      value;
 } list_elem_t;
 
-const list_elem_t LIST_POISON = {"pzn_data", -1};
-
-const int DEFAULT_LIST_ELEM_VALUE = 1;
+const list_elem_t   LIST_POISON             = {"pzn_data", -1};
+const size_t        DEFAULT_LIST_SIZE       = 1;
+const int           DEFAULT_LIST_ELEM_VALUE = 1;
 
 #endif // CONFIG_H
