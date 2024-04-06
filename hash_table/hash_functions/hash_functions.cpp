6c79a6282746aa47481f7425db66a89c20589a48
UjeNeTORT <tikhonovty@gmail.com>
2024-04-07 02:30:13 +0300

    add hash funcs

--- hash_table/hash_functions/hash_functions.cpp
+++ hash_table/hash_functions/hash_functions.cpp
@@ -7,10 +7,51 @@ volatile hash_t  CalcStrHashReturnZero    (ht_key_t key)
     return 0;
 }
 
-volatile hash_t  CalcStrHashFirstLetter   (ht_key_t key);
-volatile hash_t  CalcStrHashWordLength    (ht_key_t key);
-volatile hash_t  CalcStrHashControlSum    (ht_key_t key);
-volatile hash_t  CalcStrHashSumOverLength (ht_key_t key);
-volatile hash_t  CalcStrHashDedFormula    (ht_key_t key);
+volatile hash_t  CalcStrHashFirstLetter   (ht_key_t key)
+{
+    return ((const char *) key)[0];
+}
 
-volatile hash_t  CalcStrHashCRC32         (ht_key_t key);
+volatile hash_t  CalcStrHashWordLength    (ht_key_t key)
+{
+    return strlen ((const char *) key);
+
+}
+
+volatile hash_t  CalcStrHashControlSum    (ht_key_t key)
+{
+    hash_t sum = 0;
+
+    while (*((const char *) key))
+    {
+        sum += *((const char *) key);
+        key++;
+    }
+
+    return sum;
+}
+
+volatile hash_t  CalcStrHashSumOverLength (ht_key_t key)
+{
+    hash_t sum = 0;
+
+    ht_key_t key_init = key;
+
+    while (*((const char *) key))
+    {
+        sum += *((const char *) key);
+        key++;
+    }
+
+    return sum / strlen ((const char *) key_init);
+}
+
+volatile hash_t  CalcStrHashDedFormula    (ht_key_t key)
+{
+    // what the fuck i cant read my conspect
+}
+
+volatile hash_t  CalcStrHashCRC32         (ht_key_t key)
+{
+    return crc32 (0, (const unsigned char *) key, strlen ((const char *) key));
+}
--- hash_table/hash_functions/hash_functions.h
+++ hash_table/hash_functions/hash_functions.h
@@ -3,6 +3,8 @@
 
 #include <assert.h>
 #include <stdlib.h>
+#include <string.h>
+#include <zlib.h>
 
 #include "../../config.h"
 
