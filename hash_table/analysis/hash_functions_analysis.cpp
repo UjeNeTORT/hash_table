2511616cd1b92eb9ba82ea43f6aab060470a8f4b
UjeNeTORT <tikhonovty@gmail.com>
2024-04-07 14:39:47 +0300

    more convenient analysis func

--- hash_table/analysis/hash_functions_analysis.cpp
+++ hash_table/analysis/hash_functions_analysis.cpp
@@ -1,7 +1,58 @@
 #include "hash_functions_analysis.h"
 
+int AnalyzeMultipleHashFuncsFile (const char * const * const res_paths,
+                                  const char * const         preprocessed_target_data_path,
+                                  const hash_func_ptr_t      *hash_funcs,
+                                  const size_t               hash_table_size,
+                                  const size_t               n_hash_functions)
+{
+    FILE *preprocessed_target_data_file = fopen (preprocessed_target_data_path, "rb");
+    int fsize = GetFileSize (preprocessed_target_data_file);
+
+    char *preprocessed_target_data_buf = (char *) calloc (fsize + 1, sizeof (char));
+
+    fread (preprocessed_target_data_buf, sizeof (char), fsize, preprocessed_target_data_file);
+
+    AnalyzeMultipleHashFunctions (res_paths, preprocessed_target_data_buf, fsize + 1, hash_funcs, 1001, n_hash_functions);
+
+    fclose (preprocessed_target_data_file);
+    free (preprocessed_target_data_buf);
+
+    return 0;
+}
+
+int AnalyzeMultipleHashFunctions (const char * const    *analysis_results_paths,
+                                  const char            *preprocessed_target_data,
+                                  size_t                target_data_size,
+                                  const hash_func_ptr_t *hash_functions,
+                                  size_t                hash_table_size,
+                                  size_t                n_hash_functions)
+{
+    assert (analysis_results_paths);
+    assert (preprocessed_target_data);
+    assert (hash_functions);
+
+    for (int n_hf = 0; n_hf < n_hash_functions; n_hf++)
+    {
+        PrintProgressBar (n_hf, n_hash_functions);
+
+        AnalyzeHashFunction (analysis_results_paths[n_hf],
+                             preprocessed_target_data,
+                             target_data_size,
+                             hash_functions[n_hf],
+                             hash_table_size);
+
+        PrintProgressBar (n_hf + 1, n_hash_functions);
+    }
+
+    fprintf (stderr, "\n");
+
+    return 0;
+}
+
 int AnalyzeHashFunction (const char * const analysis_results_path,
-                         char               *preprocessed_target_data,
+                         const char         *preprocessed_target_data,
+                         size_t             target_data_size,
                          hash_func_ptr_t    hash_function,
                          size_t             hash_table_size)
 {
@@ -13,7 +64,10 @@ int AnalyzeHashFunction (const char * const analysis_results_path,
 
     HashTable *hash_table = HashTableCtor (hash_table_size, hash_function);
 
+    char *target_data_copy = (char *) calloc (target_data_size, sizeof (char));
+
     FILE *results_file = fopen (analysis_results_path, "wb");
+
     if (!results_file)
     {
         ERROR ("Couldnt open file \"%s\"", analysis_results_path);
@@ -21,7 +75,9 @@ int AnalyzeHashFunction (const char * const analysis_results_path,
         goto cleanup_and_return;
     };
 
-    HashTableLoadTargetData (hash_table, preprocessed_target_data, '\n');
+    strcpy (target_data_copy, preprocessed_target_data);
+
+    HashTableLoadTargetData (hash_table, target_data_copy, '\n');
 
     fprintf (results_file, "%s, %s\n", "id", "n");
     for (int id = 0; id < hash_table_size; id++)
--- hash_table/analysis/hash_functions_analysis.h
+++ hash_table/analysis/hash_functions_analysis.h
@@ -8,8 +8,22 @@
 #include "../hash_table.h"
 #include "../hash_functions/hash_functions.h"
 
+int AnalyzeMultipleHashFuncsFile (const char * const * const res_paths,
+                                  const char * const         preprocessed_target_data_path,
+                                  const hash_func_ptr_t      *hash_funcs,
+                                  const size_t               hash_table_size,
+                                  const size_t               n_hash_functions);
+
+int AnalyzeMultipleHashFunctions (const char * const    *analysis_results_paths,
+                                  const char            *preprocessed_target_data,
+                                  size_t                target_data_size,
+                                  const hash_func_ptr_t *hash_functions,
+                                  size_t                hash_table_size,
+                                  size_t                n_hash_functions);
+
 int AnalyzeHashFunction (const char * const analysis_results_path,
-                         char               *preprocessed_target_data,
+                         const char         *preprocessed_target_data,
+                         size_t             target_data_size,
                          hash_func_ptr_t    hash_function,
                          size_t             hash_table_size);
 
